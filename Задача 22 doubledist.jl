e16014df22b5c4cb3ce9c5b25a3ee735b8ea9234
mishelpogorelov <Mishelpogorelov@yandex.ru>
2024-12-23 18:39:35 +0300

    Add files via upload

--- Задача 22 doubledist.jl
+++ Задача 22 doubledist.jl
@@ -33,3 +33,35 @@ function doubledist1!(robot, side)
         putmarker!(robot)
     end
 end
+
+
+function numstepstolimit!(robot, side)
+    isborder(robot, side) && return 0
+    move!(robot, side)
+    return numstepstolimit!(robot, side) + 1
+end
+    
+function attemptmove!(robot, side, max_num_steps::Integer, num_steps =0 ::Integer)
+    max_num_steps == 0 && return num_steps
+    if trymove!(robot, side)
+        return attemptmove!(robot, side, max_num_steps - 1, num_steps+ 1)
+    else
+        return num_steps
+    end
+end
+function trymove!(robot, side)
+    isborder(robot, side) && return false
+    move!(robot, side)
+    return true
+end
+    
+function trydoubledist!(robot, side)
+    n = numstepstolimit!(robot, side)
+    m = attemptmove!(robot, inverse(side), 2n)
+    if m == 2n
+        return true
+    else
+        move!(robot, side, m-n)
+        return false
+    end
+end
